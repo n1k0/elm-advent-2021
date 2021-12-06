fbd8533529cba7d60a7272bd81b8d41630c448b7
Nicolas Perriault <nicolas@perriault.net>
2021-12-06 17:35:48 +0100

    Add content to homepage.

--- src/Pages/Home.elm
+++ src/Pages/Home.elm
@@ -1,6 +1,10 @@
 module Pages.Home exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Markdown.Html as MdHtml
+import Markdown.Parser as Parser
+import Markdown.Renderer exposing (Renderer, defaultHtmlRenderer)
 import Shared exposing (Shared)
 import Spa.Page
 import View exposing (View)
@@ -10,8 +14,32 @@ page shared =
     Spa.Page.static (view shared)
 
 
+renderer : Renderer (Html msg)
+renderer =
+    { defaultHtmlRenderer
+        | html =
+            MdHtml.oneOf
+                [ MdHtml.tag "intro" (div [])
+                ]
+    }
+
+
+intro : Result String (List (Html msg))
+intro =
+    "<intro>This is [my](https://nicolas.perriault.net/) attempt at the 2021 [Advent of Code](https://adventofcode.com/) in [Elm](https://elm-lang.org/).</intro>"
+        |> Parser.parse
+        |> Result.mapError (List.map Parser.deadEndToString >> String.join "\n")
+        |> Result.andThen (Markdown.Renderer.render renderer)
+
+
 view : Shared -> View msg
 view _ =
     { title = "Home"
-    , body = [ text "This is the homepage" ]
+    , body =
+        case intro of
+            Ok html ->
+                html
+
+            Err _ ->
+                [ text "error" ]
     }
