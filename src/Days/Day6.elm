bf38696bb3fa903fce59d7aa2b1d4ce4539f9923
Nicolas Perriault <nicolas@perriault.net>
2021-12-11 16:28:03 +0100

    Simplify implementation for Day 6, part 1.

--- src/Days/Day6.elm
+++ src/Days/Day6.elm
@@ -7,44 +7,36 @@ type alias Population =
     List Int
 
 
-type alias Days =
-    List Population
-
-
 part1 : Int
 part1 =
     -- Note: data is the provided input string
     sampleData
         |> String.split ""
         |> List.filterMap String.toInt
-        |> List.singleton
-        |> List.foldl (processDays 80) []
-        |> List.head
-        |> Maybe.map List.length
-        |> Maybe.withDefault -1
+        |> run 80
 
 
-processDays : Int -> Population -> Days -> Days
-processDays counter population days =
-    let
-        new =
-            population |> List.map evolve |> List.concat
-    in
-    if counter == 1 then
-        new :: days
+run : Int -> Population -> Int
+run days pop =
+    List.range 1 days
+        |> List.foldl (\_ -> process) pop
+        |> List.length
 
-    else
-        processDays (counter - 1) new (new :: days)
+
+process : Population -> Population
+process population =
+    population
+        |> List.map evolve
+        |> List.concat
 
 
 evolve : Int -> List Int
 evolve n =
-    case n of
-        0 ->
-            [ 6, 8 ]
+    if n == 0 then
+        [ 6, 8 ]
 
-        x ->
-            [ x - 1 ]
+    else
+        [ n - 1 ]
 
 
 
@@ -56,24 +48,6 @@ answer =
     String.join "\n"
         [ "Part1: " ++ String.fromInt part1
         ]
-
-
-debugDays : Days -> String
-debugDays days =
-    days
-        |> List.indexedMap
-            (\index line ->
-                "Day "
-                    ++ String.fromInt index
-                    ++ ": "
-                    ++ String.fromInt (List.length line)
-                    ++ " - "
-                    ++ (line
-                            |> List.map String.fromInt
-                            >> String.join ","
-                       )
-            )
-        |> String.join "\n"
 
 
 sampleData : String
@@ -90,21 +64,21 @@ The sea floor is getting steeper. Maybe the sleigh keys got carried this way?
 
 A massive school of glowing lanternfish swims past. They must spawn quickly to reach such large numbers - maybe exponentially quickly? You should model their growth rate to be sure.
 
-Although you know nothing about this specific species of lanternfish, you make some guesses about their attributes. Surely, each lanternfish creates a new lanternfish once every 7 days.
+Although you know nothing about this specific species of lanternfish, you make some guesses about their attributes. Surely, each lanternfish creates a new lanternfish once every `7` days.
 
-However, this process isn't necessarily synchronized between every lanternfish - one lanternfish might have 2 days left until it creates another lanternfish, while another might have 4. So, you can model each fish as a single number that represents the number of days until it creates a new lanternfish.
+However, this process isn't necessarily synchronized between every lanternfish - one lanternfish might have `2` days left until it creates another lanternfish, while another might have `4`. So, you can model each fish as a single number that represents the number of days until it creates a new lanternfish.
 
 Furthermore, you reason, a new lanternfish would surely need slightly longer before it's capable of producing more lanternfish: two more days for its first cycle.
 
-So, suppose you have a lanternfish with an internal timer value of 3:
+So, suppose you have a lanternfish with an internal timer value of `3`:
 
-- After one day, its internal timer would become 2.
-- After another day, its internal timer would become 1.
-- After another day, its internal timer would become 0.
-- After another day, its internal timer would reset to 6, and it would create a new lanternfish with an internal timer of 8.
-- After another day, the first lanternfish would have an internal timer of 5, and the second lanternfish would have an internal timer of 7.
+- After one day, its internal timer would become `2`.
+- After another day, its internal timer would become `1`.
+- After another day, its internal timer would become `0`.
+- After another day, its internal timer would reset to `6`, and it would create a new lanternfish with an internal timer of `8`.
+- After another day, the first lanternfish would have an internal timer of 5, and the second lanternfish would have an internal timer of `7`.
 
-A lanternfish that creates a new fish resets its timer to 6, not 7 (because 0 is included as a valid timer value). The new lanternfish starts with an internal timer of 8 and does not start counting down until the next day.
+A lanternfish that creates a new fish resets its timer to `6`, not `7` (because `0` is included as a valid timer value). The new lanternfish starts with an internal timer of `8` and does not start counting down until the next day.
 
 Realizing what you're trying to do, the submarine automatically produces a list of the ages of several hundred nearby lanternfish (your puzzle input). For example, suppose you were given the following list:
 
@@ -132,11 +106,21 @@ This list means that the first fish has an internal timer of 3, the second fish 
     After 17 days: 0,1,0,5,6,0,1,2,2,3,0,1,2,2,2,3,3,4,4,5,7,8
     After 18 days: 6,0,6,4,5,6,0,1,1,2,6,0,1,1,1,2,2,3,3,4,6,7,8,8,8,8
 
-Each day, a 0 becomes a 6 and adds a new 8 to the end of the list, while each other number decreases by 1 if it was present at the start of the day.
+Each day, a `0` becomes a `6` and adds a new `8` to the end of the list, while each other number decreases by `1` if it was present at the start of the day.
 
-In this example, after 18 days, there are a total of 26 fish. After 80 days, there would be a total of 5934.
+In this example, after `18` days, there are a total of `26` fish. After `80` days, there would be a total of `5934`.
 
 Find a way to simulate lanternfish. How many lanternfish would there be after 80 days?
+
+--- Part Two ---
+
+Suppose the lanternfish live forever and have unlimited food and space. Would they take over the entire ocean?
+
+After `256` days in the example above, there would be a total of `26984457539` lanternfish!
+
+How many lanternfish would there be after `256` days?
+
+
 """
 
 
